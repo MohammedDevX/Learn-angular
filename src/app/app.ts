13094086b85fc3180a2de2ca31522883318fa0c8
KAKACHI123EFZEF <125193387+KAKACHI123EFZEF@users.noreply.github.com>
2026-01-31 15:39:27 +0100

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// @Component : is a decodator that give the element an behavior in run time, for ex here we said this
+// class must be as component
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
